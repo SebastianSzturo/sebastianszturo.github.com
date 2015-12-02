f10c980c338b792b57904fbac6313e9f3572ca7f
Sebastian Szturo <s.szturo@me.com>
2015-12-02 18:44:39 +0100

    Open external links in new tab

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,54 @@
 // To make images retina, add a class "2x" to the img element
 // and add a <image-name>@2x.png image. Assumes jquery is loaded.
- 
+
 function isRetina() {
 	var mediaQuery = "(-webkit-min-device-pixel-ratio: 1.5),\
 					  (min--moz-device-pixel-ratio: 1.5),\
 					  (-o-min-device-pixel-ratio: 3/2),\
 					  (min-resolution: 1.5dppx)";
- 
+
 	if (window.devicePixelRatio > 1)
 		return true;
- 
+
 	if (window.matchMedia && window.matchMedia(mediaQuery).matches)
 		return true;
- 
+
 	return false;
 };
- 
- 
+
+
 function retina() {
-	
+
 	if (!isRetina())
 		return;
-	
+
 	$("img.2x").map(function(i, image) {
-		
+
 		var path = $(image).attr("src");
-		
+
 		path = path.replace(".png", "@2x.png");
 		path = path.replace(".jpg", "@2x.jpg");
-		
+
 		$(image).attr("src", path);
 	});
 };
- 
-$(document).ready(retina);
+
+// Open external links in new window
+var externalLinks = function(){
+  var host = location.host;
+
+  $('body').on('click', 'a', function(e){
+    var href = this.href,
+      link = href.replace(/https?:\/\/([^\/]+)(.*)/, '$1');
+
+    if (link != '' && link != host && !$(this).hasClass('fancybox')){
+      window.open(href);
+      e.preventDefault();
+    }
+  });
+};
+
+$(document).ready(function(){
+  retina();
+  externalLinks();
+});
